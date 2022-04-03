400f642b0fc584d38a16262953eb30fef9a55d5b
wuxing <logerror@xy.hfut.edu.cn>
2022-04-03 18:11:34 +0800

    add ccls & cmake

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,6 +3,28 @@ if not status_ok then
 	return
 end
 
+local servers = {
+	sumneko_lua = require("user.lsp.settings.sumneko_lua"), -- lua/lsp/config/lua.lua
+	jsonls = require("user.lsp.settings.jsonls")
+	cmake = require("user.lsp.settings.cmake")
+	ccls = require("user.lsp.settings.ccls")
+	-- rust_analyzer = require("lsp.lang.rust"),
+	-- jsonls = require("lsp.lang.json"),
+	-- tsserver = require("lsp.config.ts"),
+	-- remark_ls = require("lsp.lang.markdown"),
+	-- html = {},
+}
+
+for name, _ in pairs(servers) do
+	local server_is_found, server = lsp_installer.get_server(name)
+	if server_is_found then
+	  if not server:is_installed() then
+		print("Installing " .. name)
+		server:install()
+	  end
+	end
+  end
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
@@ -11,15 +33,10 @@ lsp_installer.on_server_ready(function(server)
 		capabilities = require("user.lsp.handlers").capabilities,
 	}
 
-	 if server.name == "jsonls" then
-	 	local jsonls_opts = require("user.lsp.settings.jsonls")
-	 	opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	 end
-
-	 if server.name == "sumneko_lua" then
-	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
-	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	 end
+	local config = servers[server.name]
+	if config != nil then
+		opts = vim.tbl_deep_extend("force", config, opts)
+	end
 
 	-- This setup() function is exactly the same as lspconfig's setup function.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
